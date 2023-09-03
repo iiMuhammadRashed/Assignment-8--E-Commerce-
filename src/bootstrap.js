9524689cf250ff3bd0f73302ce0fe4add9ad699f
Beka <dodi231231@gmail.com>
2023-09-03 20:24:08 +0300

    add UI with ejs

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -5,13 +5,36 @@ import subCategoryRouter from './modules/subCategory/subCategory.routes.js';
 import userRouter from './modules/user/user.routes.js';
 import morgan from 'morgan';
 import { AppError } from './utils/AppError.js';
-export function bootstrap(app) {
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export function bootstrap(app, express) {
   app.use(morgan('dev'));
+
+  // Static Files
+  app.use(express.static('public'));
+  app.use('/css', express.static(__dirname + 'public/css'));
+  app.use('/images', express.static(__dirname + 'public/images'));
+  app.use('/js', express.static(__dirname + 'public/js'));
+  console.log(__dirname);
+
+  // Set View
+  app.set('views', './views');
+  app.set('view engine', 'ejs');
+  app.get('/', (req, res) => {
+    res.render('index');
+  });
+
+  // APIs Endpoints
+  app.use(express.json());
   app.use('/api/v1/categories', categoryRouter);
   app.use('/api/v1/subCategories', subCategoryRouter);
   app.use('/api/v1/brands', brandRouter);
   app.use('/api/v1/products', productRouter);
   app.use('/api/v1/users', userRouter);
+  app.use('/api/v1/auth', userRouter);
   app.all('*', (req, res, next) => {
     next(new AppError(`Invalid endpoint ${req.originalUrl}`, 404));
   });
